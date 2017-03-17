1646b68ab233a54cff0323776c51ed973d52508d
Shanu <sshanukr@gmail.com>
2017-03-17 13:10:38 +0530

    add member fixed

--- member/admin.py
+++ member/admin.py
@@ -7,10 +7,14 @@ from itertools import chain
 class MemberAdmin(admin.ModelAdmin):
 	def formfield_for_manytomany(self, db_field, request, **kwargs):
 		if db_field.name == "books":
-			self_pub_id = request.resolver_match.args[0]
-			query1 = Post.objects.filter(memberid=None)
-			query2 = Post.objects.filter(memberid=self_pub_id)
-			kwargs["queryset"] =query2 | query1
+			try:
+				self_pub_id = request.resolver_match.args[0]
+				query1 = Post.objects.filter(memberid=None)
+				query2 = Post.objects.filter(memberid=self_pub_id)
+				kwargs["queryset"] =query2 | query1
+			except IndexError:
+				query1 = Post.objects.filter(memberid=None)
+				kwargs["queryset"] =query1
 		return super(MemberAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 	search_fields = ('Name','id', 'RollNo',)
 	list_display = ('Name','id', 'RollNo','EmailID', 'Fine','Slots',)
